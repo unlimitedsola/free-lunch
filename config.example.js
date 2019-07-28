2dca9681b3e018eefb6c4ce12bd185fa5f63d19b
Sola <dev@sola.love>
2019-07-28 21:09:31 +0800

    add log in instructions

--- config.example.js
+++ config.example.js
@@ -1,6 +1,6 @@
 module.exports = {
   // if you don't know what this means, just replace latitude and longitude and you're good to go.
   url: 'https://h5.ele.me/restapi/shopping/v3/restaurants?latitude=22.523640&longitude=113.391071&extras[]=activities&extras[]=tags&extra_filters=home&quality_union=1&activity_types[]=3&rank_id=&terminal=h5',
-  // your session id, can be obtained from your cookies.
+  // your session id, can be obtained from your cookies at https://h5.ele.me/.
   sid: ''
 }
