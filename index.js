48aa488957acbb618a044609dc8d8f58659f8203
Sola <dev@sola.love>
2019-07-28 09:02:10 +0800

    adjust limit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const fetchRestaurants = require('./request')
 
 async function main () {
   let restaurants = await fetchRestaurants(100)
-  let result = sorter(restaurants).filter(e => e.minCost.cost < 20)
+  let result = sorter(restaurants).filter(e => e.minCost.cost < 25)
   result.forEach(e => console.log(`${e.name}: discount(${e.minCost.discount}) + red-packet(${e.minCost.redPacket}) = ${e.minCost.cost}`))
 }
 
